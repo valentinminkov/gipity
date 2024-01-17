4f0649c5637c0ffbcfbfb559e2c22c92bc02f5cb
valentin <valentin.minkov@mycs.com>
2024-01-17 23:43:05 +0100

    first commit -m

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <b>G</b>i<b>p</b>i<b>t</b>y
+
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <MenuLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import MenuLink from 'components/MenuLink.vue';
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'radio_button_unchecked',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'radio_button_unchecked',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'radio_button_unchecked',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'radio_button_unchecked',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'radio_button_unchecked',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'radio_button_unchecked',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'radio_button_unchecked',
+    link: 'https://awesome.quasar.dev'
+  }
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    MenuLink
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="column items-center justify-end">
+    <InputSection class="" placeholder="Write a message" active></InputSection>
+  </q-page>
+</template>
+
+<script lang="ts">
+import InputSection from 'components/InputSection.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { InputSection },
+  setup() {
+
+    return {};
+  }
+});
+</script>
