21d9902e00c1117efd3e41c9f0a37626b23c215f
minkowalentin <minkowalentin@proton.me>
2024-01-18 21:56:33 +0100

    cleanup

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,14 +6,13 @@
 
         <q-toolbar-title>
           <b>G</b>i<b>p</b>i<b>t</b>y
-
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <MenuLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+
       </q-list>
     </q-drawer>
 
@@ -25,65 +24,16 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import MenuLink from 'components/MenuLink.vue';
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'radio_button_unchecked',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'radio_button_unchecked',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'radio_button_unchecked',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'radio_button_unchecked',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'radio_button_unchecked',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'radio_button_unchecked',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'radio_button_unchecked',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    MenuLink
-  },
 
   setup() {
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
