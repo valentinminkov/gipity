bd8af9bec663c1891ecdc8777eaa291e8a01f74b
minkowalentin <minkowalentin@proton.me>
2024-01-23 23:48:58 +0100

    change router config to history

--- quasar.config.js
+++ quasar.config.js
@@ -53,7 +53,7 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16',
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -80,6 +80,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      vueRouterMode: 'history',
       open: true, // opens browser window automatically
     },
 
