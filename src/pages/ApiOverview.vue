a8c619aa231a1be53378e966595b6273029a0cf9
minkowalentin <minkowalentin@proton.me>
2024-01-23 23:48:34 +0100

    create dummy overview wwcomponent

---
+++ src/pages/ApiOverview.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quos molestias totam asperiores quo eos ex eius
+    sed ut necessitatibus doloribus vero, aliquid placeat iure nobis dolorum temporibus blanditiis aspernatur!</p>
+  <!-- content -->
+</template>
+
+<script lang="ts" setup>
+
+</script>
