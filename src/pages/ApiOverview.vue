2aaa53f7b33b774e55b1c35c30b5ccc6bbd15189
minkowalentin <minkowalentin@proton.me>
2024-01-25 21:31:48 +0100

    fetch models instead of using only dummydata

--- src/pages/ApiOverview.vue
+++ src/pages/ApiOverview.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page padding>
     <q-table :rows="tableData" :columns="columns" row-key="id" virtual-scroll binary-state-sort flat bordered
-      title="Models Overview">
+      :rows-per-page-options="[10, tableData?.length]" title="Models Overview">
       <template v-slot:body="props">
         <q-tr :props="props">
           <q-td key="id" :props="props">{{ props.row.id }}</q-td>
@@ -17,7 +17,8 @@
 
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { useOpenai } from '../composables/useOpenai'
+import { onMounted, ref } from 'vue';
 
 interface Column {
   name: string;
@@ -29,6 +30,9 @@ interface Column {
   format?: (val: any) => string;
 }
 
+const dummyData = { 'options': { 'method': 'get', 'path': '/models' }, 'response': { 'size': 0, 'timeout': 0 }, 'body': { 'object': 'list', 'data': [{ 'id': 'gpt-3.5-turbo-1106', 'object': 'model', 'created': 1698959748, 'owned_by': 'system' }, { 'id': 'dall-e-3', 'object': 'model', 'created': 1698785189, 'owned_by': 'system' }, { 'id': 'whisper-1', 'object': 'model', 'created': 1677532384, 'owned_by': 'openai-internal' }, { 'id': 'dall-e-2', 'object': 'model', 'created': 1698798177, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo-16k', 'object': 'model', 'created': 1683758102, 'owned_by': 'openai-internal' }, { 'id': 'gpt-3.5-turbo-16k-0613', 'object': 'model', 'created': 1685474247, 'owned_by': 'openai' }, { 'id': 'text-embedding-ada-002', 'object': 'model', 'created': 1671217299, 'owned_by': 'openai-internal' }, { 'id': 'tts-1-hd-1106', 'object': 'model', 'created': 1699053533, 'owned_by': 'system' }, { 'id': 'tts-1-hd', 'object': 'model', 'created': 1699046015, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo-instruct', 'object': 'model', 'created': 1692901427, 'owned_by': 'system' }, { 'id': 'davinci-002', 'object': 'model', 'created': 1692634301, 'owned_by': 'system' }, { 'id': 'babbage-002', 'object': 'model', 'created': 1692634615, 'owned_by': 'system' }, { 'id': 'gpt-4-0613', 'object': 'model', 'created': 1686588896, 'owned_by': 'openai' }, { 'id': 'gpt-4-1106-preview', 'object': 'model', 'created': 1698957206, 'owned_by': 'system' }, { 'id': 'gpt-4', 'object': 'model', 'created': 1687882411, 'owned_by': 'openai' }, { 'id': 'gpt-4-vision-preview', 'object': 'model', 'created': 1698894917, 'owned_by': 'system' }, { 'id': 'tts-1-1106', 'object': 'model', 'created': 1699053241, 'owned_by': 'system' }, { 'id': 'tts-1', 'object': 'model', 'created': 1681940951, 'owned_by': 'openai-internal' }, { 'id': 'gpt-3.5-turbo-instruct-0914', 'object': 'model', 'created': 1694122472, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo', 'object': 'model', 'created': 1677610602, 'owned_by': 'openai' }, { 'id': 'gpt-3.5-turbo-0301', 'object': 'model', 'created': 1677649963, 'owned_by': 'openai' }, { 'id': 'gpt-3.5-turbo-0613', 'object': 'model', 'created': 1686587434, 'owned_by': 'openai' }] }, 'data': [{ 'id': 'gpt-3.5-turbo-1106', 'object': 'model', 'created': 1698959748, 'owned_by': 'system' }, { 'id': 'dall-e-3', 'object': 'model', 'created': 1698785189, 'owned_by': 'system' }, { 'id': 'whisper-1', 'object': 'model', 'created': 1677532384, 'owned_by': 'openai-internal' }, { 'id': 'dall-e-2', 'object': 'model', 'created': 1698798177, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo-16k', 'object': 'model', 'created': 1683758102, 'owned_by': 'openai-internal' }, { 'id': 'gpt-3.5-turbo-16k-0613', 'object': 'model', 'created': 1685474247, 'owned_by': 'openai' }, { 'id': 'text-embedding-ada-002', 'object': 'model', 'created': 1671217299, 'owned_by': 'openai-internal' }, { 'id': 'tts-1-hd-1106', 'object': 'model', 'created': 1699053533, 'owned_by': 'system' }, { 'id': 'tts-1-hd', 'object': 'model', 'created': 1699046015, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo-instruct', 'object': 'model', 'created': 1692901427, 'owned_by': 'system' }, { 'id': 'davinci-002', 'object': 'model', 'created': 1692634301, 'owned_by': 'system' }, { 'id': 'babbage-002', 'object': 'model', 'created': 1692634615, 'owned_by': 'system' }, { 'id': 'gpt-4-0613', 'object': 'model', 'created': 1686588896, 'owned_by': 'openai' }, { 'id': 'gpt-4-1106-preview', 'object': 'model', 'created': 1698957206, 'owned_by': 'system' }, { 'id': 'gpt-4', 'object': 'model', 'created': 1687882411, 'owned_by': 'openai' }, { 'id': 'gpt-4-vision-preview', 'object': 'model', 'created': 1698894917, 'owned_by': 'system' }, { 'id': 'tts-1-1106', 'object': 'model', 'created': 1699053241, 'owned_by': 'system' }, { 'id': 'tts-1', 'object': 'model', 'created': 1681940951, 'owned_by': 'openai-internal' }, { 'id': 'gpt-3.5-turbo-instruct-0914', 'object': 'model', 'created': 1694122472, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo', 'object': 'model', 'created': 1677610602, 'owned_by': 'openai' }, { 'id': 'gpt-3.5-turbo-0301', 'object': 'model', 'created': 1677649963, 'owned_by': 'openai' }, { 'id': 'gpt-3.5-turbo-0613', 'object': 'model', 'created': 1686587434, 'owned_by': 'openai' }], 'object': 'list' }
+const { getModels } = useOpenai();
+const tableData = ref(formatTableData(dummyData.data));
 const columns: Column[] = [
   { name: 'id', required: true, label: 'ID', align: 'left', field: 'id', sortable: true },
   { name: 'object', label: 'Object', field: 'object', sortable: true },
@@ -36,15 +40,22 @@ const columns: Column[] = [
   { name: 'owned_by', label: 'Owned By', field: 'owned_by', sortable: true },
 ];
 
+onMounted(() => {
+  setModels();
+});
 
-const dummyData = { 'options': { 'method': 'get', 'path': '/models' }, 'response': { 'size': 0, 'timeout': 0 }, 'body': { 'object': 'list', 'data': [{ 'id': 'gpt-3.5-turbo-1106', 'object': 'model', 'created': 1698959748, 'owned_by': 'system' }, { 'id': 'dall-e-3', 'object': 'model', 'created': 1698785189, 'owned_by': 'system' }, { 'id': 'whisper-1', 'object': 'model', 'created': 1677532384, 'owned_by': 'openai-internal' }, { 'id': 'dall-e-2', 'object': 'model', 'created': 1698798177, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo-16k', 'object': 'model', 'created': 1683758102, 'owned_by': 'openai-internal' }, { 'id': 'gpt-3.5-turbo-16k-0613', 'object': 'model', 'created': 1685474247, 'owned_by': 'openai' }, { 'id': 'text-embedding-ada-002', 'object': 'model', 'created': 1671217299, 'owned_by': 'openai-internal' }, { 'id': 'tts-1-hd-1106', 'object': 'model', 'created': 1699053533, 'owned_by': 'system' }, { 'id': 'tts-1-hd', 'object': 'model', 'created': 1699046015, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo-instruct', 'object': 'model', 'created': 1692901427, 'owned_by': 'system' }, { 'id': 'davinci-002', 'object': 'model', 'created': 1692634301, 'owned_by': 'system' }, { 'id': 'babbage-002', 'object': 'model', 'created': 1692634615, 'owned_by': 'system' }, { 'id': 'gpt-4-0613', 'object': 'model', 'created': 1686588896, 'owned_by': 'openai' }, { 'id': 'gpt-4-1106-preview', 'object': 'model', 'created': 1698957206, 'owned_by': 'system' }, { 'id': 'gpt-4', 'object': 'model', 'created': 1687882411, 'owned_by': 'openai' }, { 'id': 'gpt-4-vision-preview', 'object': 'model', 'created': 1698894917, 'owned_by': 'system' }, { 'id': 'tts-1-1106', 'object': 'model', 'created': 1699053241, 'owned_by': 'system' }, { 'id': 'tts-1', 'object': 'model', 'created': 1681940951, 'owned_by': 'openai-internal' }, { 'id': 'gpt-3.5-turbo-instruct-0914', 'object': 'model', 'created': 1694122472, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo', 'object': 'model', 'created': 1677610602, 'owned_by': 'openai' }, { 'id': 'gpt-3.5-turbo-0301', 'object': 'model', 'created': 1677649963, 'owned_by': 'openai' }, { 'id': 'gpt-3.5-turbo-0613', 'object': 'model', 'created': 1686587434, 'owned_by': 'openai' }] }, 'data': [{ 'id': 'gpt-3.5-turbo-1106', 'object': 'model', 'created': 1698959748, 'owned_by': 'system' }, { 'id': 'dall-e-3', 'object': 'model', 'created': 1698785189, 'owned_by': 'system' }, { 'id': 'whisper-1', 'object': 'model', 'created': 1677532384, 'owned_by': 'openai-internal' }, { 'id': 'dall-e-2', 'object': 'model', 'created': 1698798177, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo-16k', 'object': 'model', 'created': 1683758102, 'owned_by': 'openai-internal' }, { 'id': 'gpt-3.5-turbo-16k-0613', 'object': 'model', 'created': 1685474247, 'owned_by': 'openai' }, { 'id': 'text-embedding-ada-002', 'object': 'model', 'created': 1671217299, 'owned_by': 'openai-internal' }, { 'id': 'tts-1-hd-1106', 'object': 'model', 'created': 1699053533, 'owned_by': 'system' }, { 'id': 'tts-1-hd', 'object': 'model', 'created': 1699046015, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo-instruct', 'object': 'model', 'created': 1692901427, 'owned_by': 'system' }, { 'id': 'davinci-002', 'object': 'model', 'created': 1692634301, 'owned_by': 'system' }, { 'id': 'babbage-002', 'object': 'model', 'created': 1692634615, 'owned_by': 'system' }, { 'id': 'gpt-4-0613', 'object': 'model', 'created': 1686588896, 'owned_by': 'openai' }, { 'id': 'gpt-4-1106-preview', 'object': 'model', 'created': 1698957206, 'owned_by': 'system' }, { 'id': 'gpt-4', 'object': 'model', 'created': 1687882411, 'owned_by': 'openai' }, { 'id': 'gpt-4-vision-preview', 'object': 'model', 'created': 1698894917, 'owned_by': 'system' }, { 'id': 'tts-1-1106', 'object': 'model', 'created': 1699053241, 'owned_by': 'system' }, { 'id': 'tts-1', 'object': 'model', 'created': 1681940951, 'owned_by': 'openai-internal' }, { 'id': 'gpt-3.5-turbo-instruct-0914', 'object': 'model', 'created': 1694122472, 'owned_by': 'system' }, { 'id': 'gpt-3.5-turbo', 'object': 'model', 'created': 1677610602, 'owned_by': 'openai' }, { 'id': 'gpt-3.5-turbo-0301', 'object': 'model', 'created': 1677649963, 'owned_by': 'openai' }, { 'id': 'gpt-3.5-turbo-0613', 'object': 'model', 'created': 1686587434, 'owned_by': 'openai' }], 'object': 'list' }
+const setModels = async () => {
+  const res = await getModels();
+  tableData.value = formatTableData(res?.data);
+}
 
-const tableData = computed(() => {
-  return dummyData.body.data.map(item => ({
+function formatTableData(data: any) {
+  if (!data) return [];
+  return data.map((item: any) => ({
     ...item,
     created: new Date(item.created * 1000).toLocaleString()
   }));
-});
+}
 
 function formatDate(val: any): string {
   if (typeof val === 'number') {
