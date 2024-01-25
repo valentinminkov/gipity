58a62dcde13d663bc30af2c6e57031ecd5ea5ae6
minkowalentin <minkowalentin@proton.me>
2024-01-25 21:30:38 +0100

    cors

--- server/app.ts
+++ server/app.ts
@@ -2,11 +2,14 @@ import dotenv from 'dotenv';
 import express, { Request, Response, NextFunction } from 'express';
 import { authenticateOpenAI } from './middlewares/authenticateOpenAI';
 import openaiRoutes from './routes/openaiRoutes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
+
+app.use(cors<Request>());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log('Time:', Date.now());
